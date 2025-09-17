239ef801d29a9ea80f1fbd82c09165b34270354a
K1ngGrim <90715907+K1ngGrim@users.noreply.github.com>
2025-09-17 20:20:01 +0200

    init ionic and capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.kinggrim.vbtracker',
+  appName: 'Volleyball Tracker',
+  webDir: 'dist/volleyball-tracker/browser',
+};
+
+export default config;
